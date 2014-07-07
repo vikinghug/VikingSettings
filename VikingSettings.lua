f6fdf1ff95fb294fbbdfcbb2b4b5c50f6da86436
Casper Christiansen <casperc1337@gmail.com>
2014-07-08 00:06:54 +0200

    Further work on dynamic settings and added shortcut to escape menu.

--- VikingSettings.lua
+++ VikingSettings.lua
@@ -109,7 +109,7 @@ local defaults = {
 -----------------------------------------------------------------------------------------------
 local VikingSettings = Apollo.GetPackage("Gemini:Addon-1.1").tPackage:NewAddon(
                                   NAME,
-                                  false,
+                                  true,
                                   {
                                     "Gemini:Logging-1.2",
                                     "GeminiColor",
@@ -117,7 +117,6 @@ local VikingSettings = Apollo.GetPackage("Gemini:Addon-1.1").tPackage:NewAddon(
                                   })
 
 function VikingSettings:OnInitialize()
-  -- setup logger
   local GeminiLogging = Apollo.GetPackage("Gemini:Logging-1.2").tPackage
   glog = GeminiLogging:GetLogger({
     level = GeminiLogging.INFO,
@@ -130,97 +129,82 @@ function VikingSettings:OnInitialize()
   local GeminiColor= Apollo.GetPackage("GeminiColor").tPackage
   self.gcolor = GeminiColor
 
-  -- setup database
   self.db = Apollo.GetPackage("Gemini:DB-1.0").tPackage:New(self, defaults)
 
-  -- load our form file
   self.xmlDoc = XmlDoc.CreateFromFile("VikingSettings.xml")
   self.xmlDoc:RegisterCallback("OnDocLoaded", self)
 end
 
------------------------------------------------------------------------------------------------
--- VikingSettings OnDocLoaded
------------------------------------------------------------------------------------------------
 function VikingSettings:OnDocLoaded()
-
   if self.xmlDoc ~= nil and self.xmlDoc:IsLoaded() then
-      self.wndMain = Apollo.LoadForm(self.xmlDoc, "VikingSettingsForm", nil, self)
-    if self.wndMain == nil then
-      Apollo.AddAddonErrorText(self, "Could not load the main window for some reason.")
-      return
-    end
-
-      self.wndMain:Show(false, true)
-
-    -- if the xmlDoc is no longer needed, you should set it to nil
-    -- self.xmlDoc = nil
-
-    -- Register handlers for events, slash commands and timer, etc.
-
     Apollo.RegisterSlashCommand("vui", "OnVikingUISlashCommand", self)
-    self.Addons = {}
-    self.AddonContainers = {}
-    self.AddonButtons = {}
 
-    self.bSettingsOpen = false
+    self.tAddons = {}
 
-
-    -- Do additional Addon initialization here
+    VikingSettings.RegisterSettings(self, "VikingSettings")
   end
 end
 
-function VikingSettings:ShowHideSettings()
-  self.bSettingsOpen = not self.bSettingsOpen
+function VikingSettings.RegisterSettings(tAddon, strAddonName)
+  if tAddon then
+    local tAddonData = 
+    {
+      tAddon = tAddon,
+      strAddonName = strAddonName
+    }
+
+    if not VikingSettings:IsAddonRegistered(strAddonName) then
+      table.insert(VikingSettings.tAddons, tAddonData)
+    else
+      glog:warn("Tried to register addon '" ..strAddonName.. "' but it was already registered.")
+    end
+  end
+end
+
+function VikingSettings:IsAddonRegistered(strAddonName)
+  for id, tAddonData in pairs(self.tAddons) do
+    if tAddonData.strAddonName == strAddonName then
+      return true
+    end
+  end
+
+  return false
+end
+
+function VikingSettings:ShowSettings(bShow)
   if not self.wndSettings then
-    -- Create Window
-    self.wndSettings = Apollo.LoadForm(self.xmlDoc, "VikingSettingsForm", nil, self)
-    self.currentID = 0
-
-    for id, addon in ipairs(self.Addons) do
-      self.CreateAddonForm(addon.parent, addon.addonName)
-    end
-  --   if Apollo.GetAddon("VikingTargetFrame") then
-  --     self.CreateAddonForm("VikingTargetFrame")
-  --   end
-  --   if Apollo.GetAddon("VikingClassResources") then
-  --     self.CreateAddonForm("VikingClassResources")
-  --   end
-
-    for id, currentAddonButton in ipairs(self.AddonButtons) do
-      currentAddonButton:SetAnchorOffsets(0, ((id-1)*40), 192, (id*40))
-    end
+    self:BuildSettingsWindow()
   end
-  if self.bSettingsOpen then
-    -- Open Window
-    SendVarToRover("self.AddonButtons[1]", self.AddonButtons[1])
-    self.AddonButtons[1]:Show(true)
-    self.AddonButtons[1]:SetCheck(true)
-    self:OnSettingsMenuButtonCheck()
-  end
-  self.wndSettings:Show(self.bSettingsOpen, false)
 
+  self.wndSettings:Show(bShow, false)
 end
 
-function VikingSettings.CreateAddonForm(parent, addonName)
-  glog:info("Create Addon Form: '"..addonName.."'")
+function VikingSettings:BuildSettingsWindow()
+  self.wndSettings = Apollo.LoadForm(self.xmlDoc, "VikingSettingsForm", nil, self)
 
-  SendVarToRover("VikingSettings", parent.xmlDoc)
+  for id, tAddonData in ipairs(self.tAddons) do
+    self.CreateAddonForm(tAddonData)
+    tAddonData.tButton:SetAnchorOffsets(0, (id - 1) * 40, 0, id * 40)
+  end
 
-  local newAddonContainer = Apollo.LoadForm(parent.xmlDoc         , "VikingSettings" , VikingSettings.wndMain:FindChild("Content") , VikingSettings)
-  local newAddonButton    = Apollo.LoadForm(VikingSettings.xmlDoc , "AddonButton"    , VikingSettings.wndMain:FindChild("Menu")    , VikingSettings)
-  newAddonButton:SetText(addonName)
-
-  table.insert(VikingSettings.AddonContainers, newAddonContainer)
-  table.insert(VikingSettings.AddonButtons, newAddonButton)
+  self.tAddons[1].tButton:SetCheck(true)
+  self:OnSettingsMenuButtonCheck()
 end
 
-function VikingSettings.RegisterSettings(parent, addonName)
-  local addon = {
-    parent    = parent,
-    addonName = addonName
-  }
-  table.insert(VikingSettings.Addons, addon)
-  return
+function VikingSettings.CreateAddonForm(tAddonData)
+  local wndAddonContainer = Apollo.LoadForm(tAddonData.tAddon.xmlDoc, "VikingSettings", VikingSettings.wndSettings:FindChild("Content"), tAddonData.tAddon)
+  local wndAddonButton    = Apollo.LoadForm(VikingSettings.xmlDoc, "AddonButton", VikingSettings.wndSettings:FindChild("Menu"), VikingSettings)
+  
+  -- attaching makes it show/hide the container according to the check state
+  wndAddonButton:AttachWindow(wndAddonContainer)
+  wndAddonButton:SetText(tAddonData.strAddonName)
+  wndAddonButton:Show(true)
+  wndAddonButton:SetCheck(false)
+
+  wndAddonContainer:Show(false)
+
+  tAddonData.tContainer = wndAddonContainer
+  tAddonData.tButton = wndAddonButton
 end
 
 -----------------------------------------------------------------------------------------------
@@ -235,92 +219,24 @@ end
 -- VikingSettings Form Functions
 -----------------------------------------------------------------------------------------------
 function VikingSettings:OnSettingsMenuButtonCheck( wndHandler, wndControl, eMouseButton )
-  for id, currentAddon in ipairs(self.Addons) do
-    -- self.AddonContainers[id]:Show(self.AddonButtons[id]:IsChecked())
-    -- if self.AddonButtons[id]:IsChecked() then
-    --   self.currentID = id
-    -- end
-  end
-  glog:info("Current ID: "..self.currentID)
-
   self.wndSettings:FindChild("Content"):SetVScrollPos(0)
   self.wndSettings:FindChild("Content"):RecalculateContentExtents()
 end
 
 -----------------------------------------------------------------------------------------------
--- TargetFrame Functions
------------------------------------------------------------------------------------------------
-function VikingSettings:OnTargetFrameMenuButtonCheck( wndHandler, wndControl, eMouseButton )
-  self.AddonContainers[self.currentID]:FindChild("Content:Player"):Show(self.AddonContainers[self.currentID]:FindChild("Menu:PlayerButton"):IsChecked())
-  self.AddonContainers[self.currentID]:FindChild("Content:Target"):Show(self.AddonContainers[self.currentID]:FindChild("Menu:TargetButton"):IsChecked())
-  self.AddonContainers[self.currentID]:FindChild("Content:Focus"):Show(self.AddonContainers[self.currentID]:FindChild("Menu:FocusButton"):IsChecked())
-
-  self.wndSettings:FindChild("Content"):SetVScrollPos(0)
-  self.AddonContainers[self.currentID]:FindChild("Content"):RecalculateContentExtents()
-end
-
-function VikingSettings:OnTargetFrameHighHealthColorButton( wndHandler, wndControl, eMouseButton )
-  local target = wndHandler:GetParent():GetParent():GetParent():GetName()
-  self.gcolor:ShowColorPicker(self, "UpdateTextColor", true, self.db.char.VikingTargetFrame[target].HighHealthColor, wndHandler, "VikingTargetFrame", target, "HighHealthColor")
-end
-
-
------------------------------------------------------------------------------------------------
--- ClassResource Functions
------------------------------------------------------------------------------------------------
-function VikingSettings:OnClassResourceMenuButtonCheck( wndHandler, wndControl, eMouseButton )
-  -- self.AddonContainers[self.currentID]:FindChild("Content:Warrior"):Show(self.AddonContainers[self.currentID]:FindChild("Menu:WarriorButton"):IsChecked())
-  -- self.AddonContainers[self.currentID]:FindChild("Content:Spellslinger"):Show(self.AddonContainers[self.currentID]:FindChild("Menu:SpellslingerButton"):IsChecked())
-  -- self.AddonContainers[self.currentID]:FindChild("Content:Esper"):Show(self.AddonContainers[self.currentID]:FindChild("Menu:EsperButton"):IsChecked())
-  -- self.AddonContainers[self.currentID]:FindChild("Content:Engineer"):Show(self.AddonContainers[self.currentID]:FindChild("Menu:EngineerButton"):IsChecked())
-  -- self.AddonContainers[self.currentID]:FindChild("Content:Stalker"):Show(self.AddonContainers[self.currentID]:FindChild("Menu:StalkerButton"):IsChecked())
-  -- self.AddonContainers[self.currentID]:FindChild("Content:Medic"):Show(self.AddonContainers[self.currentID]:FindChild("Menu:MedicButton"):IsChecked())
-
-  -- self.wndSettings:FindChild("Content"):SetVScrollPos(0)
-  -- self.AddonContainers[self.currentID]:FindChild("Content"):RecalculateContentExtents()
-end
-
-function VikingSettings:OnClassResourceResourceBarColorButton( wndHandler, wndControl, eMouseButton )
-  local class = wndHandler:GetParent():GetParent():GetParent():GetName()
-  self.gcolor:ShowColorPicker(self, "UpdateTextColor", true, self.db.char.VikingClassResources[class].ResourceColor, wndHandler, "VikingClassResources", class, "ResourceColor")
-end
-
-function VikingSettings:OnClassResourceGlowEffectButtonUp( wndHandler, wndControl, eMouseButton )
-  local class = wndHandler:GetParent():GetParent():GetParent():GetName()
-  self.db.char.VikingClassResources[class].EnableGlow = wndHandler:IsChecked()
-end
-
------------------------------------------------------------------------------------------------
 -- VikingSettings Functions
 -----------------------------------------------------------------------------------------------
--- Define general functions here
-
--- on SlashCommand "/vui"
 function VikingSettings:OnVikingUISlashCommand(strCmd, strParam)
-  if string.find(strParam, "tb") == 1 then
-    if string.find(strParam, "1") == 4 then
-      self.db.char.testbool = true
-      glog:info("testbool = true")
-    elseif string.find(strParam, "0") == 4 then
-      self.db.char.testbool = false
-      glog:info("testbool = false")
-    else
-      if self.db.char.testbool then
-        glog:info("testbool == true")
-      else
-        glog:info("testbool == false")
-      end
-    end
-  else
-    self:ShowHideSettings()-- show the window
-  end
+  self:ShowSettings(true)
 end
 
+function VikingSettings:OnConfigure()
+  self:ShowSettings(true)
+end
 
 -----------------------------------------------------------------------------------------------
 -- VikingSettingsForm Functions
 -----------------------------------------------------------------------------------------------
--- when the Close button is clicked
 function VikingSettings:OnCloseButton()
-  self:ShowHideSettings() -- hide the window
+  self:ShowSettings(false)
 end
